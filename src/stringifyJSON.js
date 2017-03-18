d8e95a2066202c81d703cc5c9a5694be4df17b1b
Jake Holtz <jahholtz@gmail.com>
2017-03-17 17:15:53 -0700

    WORKING on stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,13 +1,31 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
+// [1, 2]
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-   // if item is an array
-       // use recursion to "stringify" the array
-       // return that stringified array;
-   // if item is an object
-           // use recursion to "stringify" the object
-           // return that stringified object;
+  var output = '';
+
+  var isType = function(type) {
+    return typeof obj === type;
+  }
+
+  var addStr = function(string) {
+    output = output.concat(string);
+  }
+
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      stringifyJSON(obj[i]);
+    }
+  } else if (typeof obj === 'object') {
+    null;
+  } else if (isType('string')) {
+    addStr('"' + obj + '"');
+  } else if (isType('number') || isType('boolean')) {
+    addStr(obj.toString());
+  } else if (isType('undefined')) {
+    addStr(undefined);
+  }
+  return output;
 };
